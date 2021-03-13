99a37712a1c3094514cb2c92086d3aeb30dc33ba
Clown <1104480806@qq.com>
2021-03-13 13:07:48 +0800

    update

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-document.querySelector("body").addEventListener("touchmove", function (e) {
-  e.preventDefault();
-});
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let painting = false;
 let isTouchDevice = "ontouchstart" in document.documentElement;
 let points = [];
+let lastImg;
 let beginPoint;
 let clear = document.querySelector(".clear");
 let eraser = document.querySelector(".eraser");
+let download = document.querySelector(".download");
 let flag = false;
+let colorList = document.querySelector(".color").getElementsByTagName("li");
+let lineList = document.querySelector(".line-width").getElementsByTagName("li");
 let creatCanvas = () => {
   canvas.setAttribute("width", canvas.offsetWidth);
   canvas.setAttribute("height", canvas.offsetHeight);
-  ctx.fillStyle = "black";
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 1;
+  ctx.fillStyle = "red";
   ctx.lineCap = "round";
   ctx.lineJoin = "round";
 };
@@ -23,27 +22,19 @@ creatCanvas();
 
 if (isTouchDevice) {
   canvas.ontouchstart = (e) => {
-    const { x, y } = getPos(e);
+    const { x, y } = getPhonePos(e);
     points.push({ x, y });
     beginPoint = { x, y };
+    lastImg = ctx.getImageData(0, 50, canvas.width, canvas.height);
   };
   canvas.ontouchmove = (e) => {
-    const { x, y } = getPos(e);
+    const { x, y } = getPhonePos(e);
     points.push({ x, y });
-
-    if (points.length > 3) {
-      const lastTowPoints = points.slice(-2);
-      const controlPoint = lastTowPoints[0];
-      const endPoint = {
-        x: (lastTowPoints[0].x + lastTowPoints[1].x) / 2,
-        y: (lastTowPoints[0].y + lastTowPoints[1].y) / 2,
-      };
-      drawLine(beginPoint, controlPoint, endPoint);
-      beginPoint = endPoint;
-    }
+    getCurve(points);
   };
 } else {
   canvas.onmousedown = (e) => {
+    lastImg = ctx.getImageData(0, 50, canvas.width, canvas.height);
     painting = true;
     const { x, y } = getPos(e);
     points.push({ x, y });
@@ -52,17 +43,7 @@ if (isTouchDevice) {
       if (painting === true) {
         const { x, y } = getPos(e);
         points.push({ x, y });
-
-        if (points.length > 3) {
-          const lastTowPoints = points.slice(-2);
-          const controlPoint = lastTowPoints[0];
-          const endPoint = {
-            x: (lastTowPoints[0].x + lastTowPoints[1].x) / 2,
-            y: (lastTowPoints[0].y + lastTowPoints[1].y) / 2,
-          };
-          drawLine(beginPoint, controlPoint, endPoint);
-          beginPoint = endPoint;
-        }
+        getCurve(points);
       }
     };
   };
@@ -70,6 +51,36 @@ if (isTouchDevice) {
     painting = false;
   };
 }
+for (let i = 0; i < colorList.length; i++) {
+  colorList[i].style.backgroundColor = colorList[i].getAttribute("name");
+  colorList[i].onclick = function () {
+    for (let j = 0; j < colorList.length; j++) {
+      colorList[j].className = "tool";
+    }
+    this.className = "tool selected";
+    ctx.strokeStyle = colorList[i].getAttribute("name");
+  };
+}
+for (let i = 0; i < lineList.length; i++) {
+  lineList[i].style.borderWidth = lineList[i].getAttribute("index") + "px";
+  lineList[i].onclick = function () {
+    for (let j = 0; j < lineList.length; j++) {
+      lineList[j].className = "tool";
+    }
+    this.className = "tool selected";
+    ctx.lineWidth = lineList[i].getAttribute("index");
+  };
+}
+clear.addEventListener("click", () => {
+  creatCanvas();
+});
+
+eraser.addEventListener("click", () => {
+  ctx.putImageData(lastImg, 0, 50);
+});
+download.addEventListener("click", () => {
+  downLoadImage(canvas, 1);
+});
 
 function drawLine(beginPoint, controlPoint, endPoint) {
   ctx.beginPath();
@@ -77,16 +88,33 @@ function drawLine(beginPoint, controlPoint, endPoint) {
   ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y);
   ctx.stroke();
 }
+function getCurve(arr) {
+  if (arr.length > 3) {
+    const lastTowPoints = arr.slice(-2);
+    const controlPoint = lastTowPoints[0];
+    const endPoint = {
+      x: (lastTowPoints[0].x + lastTowPoints[1].x) / 2,
+      y: (lastTowPoints[0].y + lastTowPoints[1].y) / 2,
+    };
+    drawLine(beginPoint, controlPoint, endPoint);
+    beginPoint = endPoint;
+  }
+}
 function getPos(e) {
   return {
     x: e.clientX,
     y: e.clientY - 50,
   };
 }
-clear.addEventListener("click", () => {
-  creatCanvas();
-});
-
-eraser.addEventListener("click", (e) => {
-  flag = !flag;
-});
+function getPhonePos(e) {
+  return {
+    x: e.touches[0].clientX,
+    y: e.touches[0].clientY - 50,
+  };
+}
+function downLoadImage(canvas, name) {
+  var a = document.createElement("a");
+  a.href = canvas.toDataURL("image/png");
+  a.download = name;
+  a.click();
+}
