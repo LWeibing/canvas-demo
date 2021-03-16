3125ef7a40250ae82ce5e5a3434e53f7abcdb2c4
Clown <1104480806@qq.com>
2021-03-16 10:45:48 +0800

    update

--- main.js
+++ main.js
@@ -121,17 +121,17 @@ function downLoadImage(canvas, name) {
 }
 let overscroll = function (el) {
   el.addEventListener("touchstart", function () {
-    let top = el.scrollTop;
-    let totalScroll = el.scrollHeight;
-    let currentScroll = top + el.offsetHeight;
-    if (top === 0) {
-      el.scrollTop = 1;
+    let left = el.scrollLeft;
+    let totalScroll = el.scrollWidth;
+    let currentScroll = top + el.offsetWidth;
+    if (left === 0) {
+      el.scrollLeft = 1;
     } else if (currentScroll === totalScroll) {
-      el.scrollTop = top - 1;
+      el.scrollLeft = left - 1;
     }
   });
   el.addEventListener("touchmove", function (evt) {
-    if (el.offsetHeight < el.scrollHeight) evt._isScroller = true;
+    if (el.offsetWidth < el.scrollWidth) evt._isScroller = true;
   });
 };
 overscroll(toolBox);
